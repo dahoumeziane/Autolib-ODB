649a00045431f7cc2cd0fb32da2ad4b798937d27
nabil-otsmane <hn_otsmane@esi.dz>
2021-06-28 16:40:33 +0100

    merge: with dahou

---
+++ app/src/main/java/com/crewmates/autolibodb/LoadingActivity.kt
@@ -0,0 +1,71 @@
+package com.crewmates.autolibodb
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import io.socket.client.IO
+import io.socket.client.Socket
+import io.socket.emitter.Emitter
+import org.json.JSONObject
+import java.lang.Exception
+import java.net.URISyntaxException
+
+class LoadingActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: SharedViewModel
+    private lateinit var mSocket: Socket
+
+    private val onConnected: Emitter.Listener = Emitter.Listener {
+
+        Log.e("error", "connected!")
+
+        startActivity(Intent(this, MainActivity::class.java))
+
+    }
+
+    private val onError: Emitter.Listener = Emitter.Listener {
+        this@LoadingActivity.runOnUiThread(Runnable {
+            try {
+                val data: Exception = it[0] as Exception
+                Toast.makeText(this@LoadingActivity, data.message, Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                val data: JSONObject = it[0] as JSONObject
+
+            }
+
+        })
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_loading)
+
+        try {
+            val opts = IO.Options()
+            opts.path = "/socket"
+
+
+            mSocket = IO.socket("http://192.168.43.222:8123", opts)
+            mSocket.on("connected", onConnected)
+            mSocket.on("error", onError)
+            mSocket.on("connect_error", onError)
+            mSocket.connect()
+
+            val obj = JSONObject()
+            obj.put("id", 3)
+            mSocket.emit("connected vehicule", obj)
+        } catch(e: URISyntaxException) {
+            e.printStackTrace()
+        }
+
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mSocket.off("connected", onConnected)
+    }
+}
